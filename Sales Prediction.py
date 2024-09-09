baf5859c0e336cdba5c6cce73d097eea3bc0f0b1
Luisgil93 <luisgil@westernwyoming.edu>
2024-09-09 13:33:19 -0600

    Add files via upload

---
+++ Sales Prediction.py
@@ -0,0 +1,15 @@
+# Get the projected total sales.
+# Name Luis Gil
+# Date 09/06/24
+# Sales Prediction Programming Project
+# COSC 2409 DNT
+#
+
+# Variables to hold the sales total and the profit
+
+# Get the amount of projected sales.
+total_sales = float(input('Enter the projected sales: '))
+# Calculate the projected profit.
+profit = total_sales * 0.23
+# Print the projected profit.
+print('The profit is $', format(profit, ',.2f'))
