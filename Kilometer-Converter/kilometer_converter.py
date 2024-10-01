9e51312a0a45bcf943b5905a1e39772d1f95b322
Luisgil93 <luisgil@westernwyoming.edu>
2024-09-30 21:15:45 -0600

    Add files via upload

--- Kilometer-Converter/kilometer_converter.py
+++ Kilometer-Converter/kilometer_converter.py
@@ -1,7 +1,30 @@
 #
-# Name
-# Date
+# Name, Luis Gil
+# Date, 09/30/2024
 # Kilometer Converter Programming Project
 # COSC 2409 DNT
-#
-# Use comments liberally throughout the program. 
+
+# Variables
+kilometers = 0
+miles = 0
+# This program converts kilometers to miles.
+CONVERSION_FACTOR = 0.6214
+# The main function gets a distance in kilometers and calls
+# The show_miles function to convert it.
+def main ():
+    # Get the distance in kilometers.
+    kilometers = float(input('enter a distance in kilometers:'))
+    # Display the distance converted to miles
+    show_miles(kilometers)
+# The show_miles function converts the parameter km from
+# kilometers to miles and displays the result.
+def show_miles(km):
+    # Calculate miles.
+    miles = km *CONVERSION_FACTOR
+    # Display the miles.
+    print(km, 'kilometers equals', miles,'miles.')
+# Call the main function.
+main ()
+
+
+
